bbfa09865bfb1e22908f66373116319ec8a0dc97
Kyle Smith <kyledavidady@gmail.com>
2023-04-09 19:56:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Inkpot - Narrative Side Effects
+
+A simple FoundryVTT module that displays a message whenever a Skill Check in D&D 4e is rolled. The side effects are meant to encourage more narrative play, inspired by the [FFG Genesys](https://www.fantasyflightgames.com/en/products/genesys/products/genesys-core-rulebook/) system.
+
+The message suggests a side effect based on the d20 roll as displayed in the following table:
+
+| Roll | Side Effect |
+| --- | --- |
+| 1 | TERRIBLE |
+| 2 | AMAZING |
+| 3 | Bad |
+| 4 | Good |
+| 5 | Bad |
+| 6 | Good |
+| 7 | none |
+| 8 | none |
+| 9 | none |
+| 10 | none |
+| 11 | none |
+| 12 | none |
+| 13 | none |
+| 14 | none |
+| 15 | Bad |
+| 16 | Good |
+| 17 | Bad |
+| 18 | Good |
+| 19 | TERRIBLE |
+| 20 | AMAZING |
---
+++ module/inkpot.js
@@ -0,0 +1,58 @@
+const AMAZING_SIDEEFFECT = '<strong>AMAZING!</strong>';
+const TERRIBLE_SIDEEFFECT = '<strong>TERRIBLE...</strong>';
+const GOOD_SIDEEFFECT = 'Good';
+const BAD_SIDEEFFECT = 'Bad';
+const DEFAULT_SIDEEFFECT = '<em>none</em>';
+const SIDEEFFECTS = {
+  20: AMAZING_SIDEEFFECT,
+  19: TERRIBLE_SIDEEFFECT,
+  18: GOOD_SIDEEFFECT,
+  17: BAD_SIDEEFFECT,
+  16: GOOD_SIDEEFFECT,
+  15: BAD_SIDEEFFECT,
+  // 14: DEFAULT_SIDEEFFECT,
+  // ...
+  // 7: DEFAULT_SIDEEFFECT,
+  6: GOOD_SIDEEFFECT,
+  5: BAD_SIDEEFFECT,
+  4: GOOD_SIDEEFFECT,
+  3: BAD_SIDEEFFECT,
+  2: AMAZING_SIDEEFFECT,
+  1: TERRIBLE_SIDEEFFECT
+};
+
+const SKILLS = [
+  'acrobatics',
+  'arcana',
+  'athletics',
+  'bluff',
+  'diplomacy',
+  'dungeoneering',
+  'endurance',
+  'healing',
+  'history',
+  'insight',
+  'intimidation',
+  'nature',
+  'perception',
+  'religion',
+  'stealth',
+  'streetwise',
+  'thievery',
+];
+
+async function displayNarrativeSideEffect(entity, options, userId) {
+  const skillRegex = new RegExp(' uses (' + SKILLS.join('|') + ')\.$', 'i')
+  if (skillRegex.test(entity.flavor || '')) {
+    const dieResult = entity.rolls[0].dice[0].total;
+    const outcome = SIDEEFFECTS[dieResult] || DEFAULT_SIDEEFFECT;
+    ChatMessage.create({
+      user: entity.user.id,
+      speaker: entity.speaker,
+      flavor: `Narrative Side Effect`,
+      content: "&#10070; " + outcome + " &#10070;"
+    });
+  }
+}
+
+Hooks.on("createChatMessage", displayNarrativeSideEffect);
